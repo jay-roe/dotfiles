1d3ecebaa63eb1f6f0cb430804d56af2fcfe48cc
Jay Roebuck <roebuck.jayc+github@gmail.com>
2024-06-19 21:07:02 -0400

    add nix home manager

--- flake.nix
+++ flake.nix
@@ -3,17 +3,33 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
-  };
-
-  outputs = { self, nixpkgs, ... }:
-    let
-      lib = nixpkgs.lib;
-    in {
-    nixosConfigurations = {
-      enterprise = lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
-      };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      # System configuration
+      nixosConfigurations = {
+        enterprise = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+        };
+      };
+      
+      # User configuration
+      homeConfigurations = {
+        jay = home-manager.lib.homeManagerConfiguration {
+	  inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+
+      };
+    };
 }
